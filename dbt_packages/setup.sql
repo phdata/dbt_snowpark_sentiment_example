5bbc972b237fbcc61bd3671ecb63b58a16222bce
Nick Pileggi <npileggi@phdata.io>
2023-04-10 13:20:01 +0000

    Fix setup

--- dbt_packages/setup.sql
+++ dbt_packages/setup.sql
@@ -1,7 +1,7 @@
 
 USE SCHEMA [SCHEMA_TO_USE];
 
-CREATE IF NOT EXISTS STAGE DBT_DEPS;
+CREATE STAGE IF NOT EXISTS DBT_DEPS;
 
 PUT file://[path_to_project]/jaffle_shop/vader_lexicon.zip
     @DBT_DEPS/nltk_data/sentiment/
